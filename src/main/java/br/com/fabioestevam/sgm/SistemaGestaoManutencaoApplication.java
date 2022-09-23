665292d034d8bbb7643fe9e1e3efe0497ca9d08c
Fábio <fabio_henrique_estevam@hotmail.com>
2022-09-23 11:29:17 -0300

    Projeto Criado

---
+++ src/main/java/br/com/fabioestevam/sgm/SistemaGestaoManutencaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabioestevam.sgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoManutencaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestaoManutencaoApplication.class, args);
+	}
+
+}
